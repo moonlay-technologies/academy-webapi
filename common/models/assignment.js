d461ae73fad055544a751af20736926f829e9701
RNErS <rajasa.silitonga@gmail.com>
2017-08-23 11:13:46 +0700

    Perubahan academy web api
    Perubahan Final

--- common/models/assignment.js
+++ common/models/assignment.js
@@ -1,5 +1,112 @@
 'use strict';
+var app = require('../../server/server');
+var codeGenerator = require("../utils/code-generator");
 
-module.exports = function(Assignment) {
+module.exports = function (Assignment) {
+    Assignment.assignmentProgress = function (id, cb) {
+        console.log(id);
+        app.models.Assignment.findById(id, { include: 'timerecords' },
+            function (err, assignments) {
+                if (err)
+                    return cb(err);
+                else {
+                    var totalPersentasi = 0.0;
+                    //   var totalDuration = 0.0; 
+                    if (assignments.budget != 0)
+                        totalPersentasi = ((assignments.elapsed / (assignments.budget * 3600)) * 100);
+                    totalPersentasi = totalPersentasi.toFixed(4);
+                    // console.log("Total Persentasi : "+ totalPersentasi);
+                    cb(null, totalPersentasi);
+                };
+            });
 
+    };
+
+    Assignment.remoteMethod("assignmentProgress",
+        {
+            accepts:
+            {
+                arg: 'id', type: 'string', required: true
+            },
+            http: { path: '/:id/persentasi', verb: "get", errorStatus: 401 },
+            returns: { arg: "Persentasi", type: "object" }
+        });
+
+    Assignment.assignmentSubtraction = function (id, cb) {
+        app.models.Assignment.findById(id, { include: 'timerecords' },
+            function (err, assignments) {
+                if (err)
+                    return cb(err);
+                else {
+                    var diffDays = 0.0;
+                    var totalDeadline = 0.0;
+                    var date2 =  new Date (assignments.deadline)//assignments.deadline
+                    var date = new Date()
+                    var time = 0.0;
+                    console.log(date)
+                    console.log(date2.getTime())
+                    
+                    if (assignments.deadline != 0) {
+                        totalDeadline = Math.abs(date2 - date);
+                        diffDays = Math.ceil(totalDeadline / (3600 * 1000 * 24)) ;
+                        time = Math.ceil(diffDays * 8)+ " hours more";
+                        console.log("Deadline:" + time)
+                        console.log("id: " + assignments.id)
+                        cb(null, time);
+                    }
+                };
+            });
+
+    };
+
+    Assignment.remoteMethod("assignmentSubtraction",
+        {
+            accepts:
+            {
+                arg: 'id', type: 'string', required: true
+            },
+            http: { path: '/:id/subtraction', verb: "get", errorStatus: 401 },
+            returns: { arg: "Subtraction", type: "date" }
+        });
+
+
+
+
+    Assignment.getElapsed = function (id, cb) {
+        app.models.Timerecord.find({ where: { assignmentId: id } },
+            function (err, timerecords) {
+                if (err)
+                    return cb(err);
+                else {
+                    console.log(timerecords);
+                    var sum = timerecords.reduce(function (last, d) {
+                        // var elapsed = (d.waktu + last)/3600;
+                        // return elapsed.toFixed(4);
+                        return d.waktu + last;
+                    }, 0);
+                    cb(null, sum);
+                }
+            })
+    }
+
+
+    Assignment.remoteMethod("getElapsed", {
+        accepts:
+        {
+            arg: 'id',
+            type: 'string',
+            required: true
+        },
+        http:
+        {
+            path: '/:id/elapsed',
+            verb: "get",
+            errorStatus: 401
+        },
+        returns:
+        {
+            arg: "Elapsed",
+            type: "decimal"
+        }
+    })
 };
--- common/models/task.js
+++ common/models/task.js
@@ -3,21 +3,52 @@ var app = require('../../server/server');
 var codeGenerator = require("../utils/code-generator");
 
 module.exports = function (Task) {
-  Task.validatesUniquenessOf('code', { message: 'code already exists' });
 
-  Task.observe("before save", (context, next) => {
-    var Backlog = app.models.Backlog;
-    var data = context.instance || context.data;
-
-    if (context.isNewInstance) {
-      data.code = codeGenerator();
-      Backlog.findById(data.backlogId, (err, backlog) => {
-        if (err)
-          return next(err);
-        data.backlogId = backlog.id;
-        data.projectId = backlog.projectId;
-      })
+Task.getElapsedbyAssignment = function (id, cb)
+{
+    app.models.Assignment.find({where: {taskId: id}}, function (err, assignments){
+    if(err || id === 0)
+        return cb(err);
+    else{
+        console.log(assignments);
+        var sum = assignments.reduce(function(last, d) {
+            return d.elapsed + last;
+        }, 0);
+        cb(null, sum);
     }
-    next();
-  })
-};
+})};
+    
+  //Mengambil task 5 hari sebelum close
+  Task.getDueTask = function(cb) {
+    var startDate = (new Date()).setHours(0,0,0,0);
+    var endDate = (new Date(new Date().getTime()+(4*24*60*60*1000))).setHours(23,59,59,0);
+    var isoStartDate = new Date(startDate).toISOString();
+    var isoEndDate = new Date(endDate).toISOString();
+    var task = app.models.Task;
+    task.find(
+      { where:
+        { close: {between: [isoStartDate, isoEndDate] }}
+      }, 
+      function(err, results) {
+        if (err) 
+          return cb(err);
+        else
+          cb(null, results);
+      })
+    };
+
+  Task.remoteMethod("getDueTask",
+  {
+    http: { path: '/dueToDay', verb: "get", errorStatus: 401 },
+    description: ["get due task where will be close in 5 days"],
+    returns: {arg: 'Task', type: 'object', root: true}
+        
+   });
+
+
+Task.remoteMethod("getElapsedbyAssignment",
+{
+    accepts:[{arg: 'id', type: 'string', required: true}],
+    http:{path: '/:id/actual', verb: "get", errorStatus: 401},
+    returns:{arg: "Actual", type:"decimal"}})
+};       
