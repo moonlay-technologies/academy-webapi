f5fbbd7cf2c66291eb0809f6b4f1744df2eab428
albertkelvinhu <albertkelvinhutapea@gmail.com>
2017-08-24 16:00:31 +0700

    memperbaiki model project.js

--- common/models/project.js
+++ common/models/project.js
@@ -5,39 +5,51 @@ module.exports = function (Project) {
   Project.validatesUniquenessOf('code');
 
   // count progress of project by id
-  Project.projectWithProgress = function(id, cb) {
-        var closedPromise = app.models.Backlog.count( {projectId : id, status: "closed"});
-        var totalPromise = app.models.Backlog.count( {projectId : id});
-        app.models.Project.findById(id, function(err, results) {
-            if (err)
-                return cb(err);
-            else {
-                var project = results;
-                Promise.all([closedPromise, totalPromise]).then(results => {
-                    project.closedBacklog = results[0];
-                    project.totalBacklog = results[1];
-                    project.progress = 0;
-                    if(project.totalBacklog == 0)
-                        cb(null, project);
-                    else {
-                        project.progress = (project.closedBacklog / project.totalBacklog * 100).toFixed(2);
-                        cb(null, project);
-                    }
-                });
-            }
+  Project.projectProgress = function(id, cb) {
+      console.log(id);
+      var closedPromise = app.models.Backlog.count( {projectId : id, status: "closed"});
+      var totalPromise = app.models.Backlog.count( {projectId : id});
+        Promise.all([closedPromise, totalPromise]).then(results => {
+            var closedBacklog = results[0];
+            var totalBacklog = results[1];
+            var result = closedBacklog / totalBacklog * 100;
+            result = result.toFixed(2);
+            cb(null, {closedBacklog, totalBacklog, result});
         });
-       
-        
   };
 
-
-  Project.remoteMethod("projectWithProgress", 
+  Project.remoteMethod("projectProgress", 
   {
         accepts: { arg: 'id', type: 'string', required: true},
-        http: { path: '/:id/projectWithProgress', verb: "get", errorStatus: 401 },
+        http: { path: '/:id/progress', verb: "get", errorStatus: 401 ,},
         description: ["get project progress"],
-        returns: {arg: "progress", type: "object", root: true}
+        returns: {arg: "progress", type: "object"}
         
   });
+
+    // get detail and count progress of project by id 
+  Project.projectDetailProgress = function(id, cb) {
+      console.log(id);
+      var projectPromise = app.models.Backlog.findById(id);
+      var closedPromise = app.models.Backlog.count( {projectId : id, status: "closed"});
+      var totalPromise = app.models.Backlog.count( {projectId : id});
+        Promise.all([projectPromise, closedPromise, totalPromise]).then(results => {
+            var project = results[0];
+            project.closedBacklog = results[1];
+            project.totalBacklog = results[2];
+            var result = project.closedBacklog / project.totalBacklog * 100;
+            project.progress = result.toFixed(2);
+            cb(null, project);
+        });
+  };
+
+  Project.remoteMethod("projectDetailProgress", 
+    {
+        accepts: { arg: 'id', type: 'string', required: true},
+        http: { path: '/:id/detailProject', verb: "get", errorStatus: 401},
+        description: ["get project detail with progress"],
+        returns: {arg: 'project', type: 'object', root: true}
+        
+    });
   
 };
