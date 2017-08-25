491d0f4d1053f8a2b25e724b7e49b30e9e86e475
surya <suryasimorangkir16@gmail.com>
2017-08-25 15:57:51 +0700

    update api project progress

--- common/models/project.js
+++ common/models/project.js
@@ -19,9 +19,9 @@ module.exports = function (Project) {
                     var completed = results[0];
                     var total = results[1];
                     var progress = 0;
-                    if(total != 0) 
-                        progress = ((completed/total) * 100).toFixed(2);
-                    
+                    if (total != 0)
+                        progress = ((completed / total) * 100).toFixed(2);
+
                     project.completedBacklog = completed;
                     project.totalBacklog = total;
                     project.progress = progress;
@@ -40,5 +40,61 @@ module.exports = function (Project) {
         description: ["get project progress by id"],
         returns: { arg: "progress", type: "object", root: true }
     });
+
+    Project.progressBacklog = function (id, cb) {
+        var dateNow = new Date();
+        var completedPromise = app.models.Backlog.count({ projectId: id, status: "closed" });
+        var planningPromise = app.models.Backlog.count({ projectId: id, status: "open", deadline: {gte: dateNow}});
+        var overDuePromise = app.models.Backlog.count({ projectId: id, status: "open", deadline: {lt: dateNow} });
+        var totalPromise = app.models.Backlog.count({ projectId: id});
+        
+        Promise.all([completedPromise, planningPromise, overDuePromise, totalPromise]).then(results => {
+            var backlogProgress ={};
+            backlogProgress.completed = results[0];
+            backlogProgress.planning = results[1];
+            backlogProgress.overDue = results[2];
+            backlogProgress.total = results[3];
+
+            cb(null, backlogProgress);
+        })
+
+    };
+
+
+    Project.remoteMethod("progressBacklog",
+    {
+        accepts: { arg: 'id', type: 'string', required: true },
+        http: { path: '/:id/backlog/progress', verb: "get", errorStatus: 401 },
+        description: ["get backlog progress"],
+        returns: { arg: "progress", type: "object", root: true }
+    });
+
+    Project.progressTask = function (id, cb) {
+        var dateNow = new Date();
+        var completedPromise = app.models.Task.count({ projectId: id, status: "closed" });
+        var planningPromise = app.models.Task.count({ projectId: id, status: "open", deadline: {gte: dateNow}});
+        var overDuePromise = app.models.Task.count({ projectId: id, status: "open", deadline: {lt: dateNow} });
+        var totalPromise = app.models.Task.count({ projectId: id});
+        
+        Promise.all([completedPromise, planningPromise, overDuePromise, totalPromise]).then(results => {
+            var tasksProgress ={};
+            tasksProgress.completed = results[0];
+            tasksProgress.planning = results[1];
+            tasksProgress.overDue = results[2];
+            tasksProgress.total = results[3];
+            cb(null, tasksProgress);
+        })
+
+    };
+
+
+    Project.remoteMethod("progressTask",
+    {
+        accepts: { arg: 'id', type: 'string', required: true },
+        http: { path: '/:id/tasks/progress', verb: "get", errorStatus: 401 },
+        description: ["get tasks progress"],
+        returns: { arg: "progress", type: "object", root: true }
+    });
+        
 
 };
